b93a2e5e05e357e12ef4d981201d54ffc9b76137
“Pradaap-SS” <pradaapss@gmail.com>
2023-06-24 19:15:00 -0500

    #6 added tab component

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,8 +5,8 @@ const state = proxy({
   color: '#EFBD48',
   isLogoTexture: true,
   isFullTexture: false,
-  logoDecal: './threejs.png',
-  fullDecal: './threejs.png',
+  logoDecal: './manchester_united.png',
+  fullDecal: './manchester_united.png',
 });
 
 export default state;
